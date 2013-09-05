0d39cf9ccac9a83a32d01fe05653750be34df580
gligorov <gligorov@4525493e-7705-40b1-a816-d608a930855b>
2013-09-05 13:52:07 +0000

    switch to pos/neg p0/p1 config files
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Urania/trunk/PhysFit/B2DXFitters@162138 4525493e-7705-40b1-a816-d608a930855b

--- data/Bs2DsKConfigForFixedParamSyst_p0.py
+++
@@ -1,8 +0,0 @@
-def getconfig() :
-
-    from Bs2DsKConfigForNominalGammaFitToys5M import getconfig as getconfig_nominal
-    baselineconfig = getconfig_nominal()
-
-    baselineconfig["calibration_p0"] += 0.014
-
-    return baselineconfig
