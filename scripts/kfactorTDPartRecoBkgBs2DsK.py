864ce4390709b9bff0a026a72266556fc0350f7a
mschille <mschille@4525493e-7705-40b1-a816-d608a930855b>
2014-01-13 14:42:53 +0000

    small changes to Suvayu's new k-factor template code: fix 'else if' -> 'elif', use standard shell/python trampoline code to get the environment right
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Urania/trunk/PhysFit/B2DXFitters@166737 4525493e-7705-40b1-a816-d608a930855b

--- scripts/kfactorTDPartRecoBkgBs2DsK.py
+++ scripts/kfactorTDPartRecoBkgBs2DsK.py
@@ -1,6 +1,91 @@
-#!/usr/bin/env python
+#!/bin/sh
+# -*- mode: python; coding: utf-8 -*-
+# vim: ft=python:sw=4:tw=78
+# --------------------------------------------------------------------------- 
 # coding=utf-8
+""":"
+# This part is run by the shell. It does some setup which is convenient to save
+# work in common use cases.
+
+# make sure the environment is set up properly
+if test -n "$CMTCONFIG" \
+         -a -f $B2DXFITTERSROOT/$CMTCONFIG/libB2DXFittersDict.so \
+     -a -f $B2DXFITTERSROOT/$CMTCONFIG/libB2DXFittersLib.so; then
+    # all ok, software environment set up correctly, so don't need to do 
+    # anything
+    true
+else
+    if test -n "$CMTCONFIG"; then
+    # clean up incomplete LHCb software environment so we can run
+    # standalone
+        echo Cleaning up incomplete LHCb software environment.
+        PYTHONPATH=`echo $PYTHONPATH | tr ':' '\n' | \
+            egrep -v "^($User_release_area|$MYSITEROOT/lhcb)" | \
+            tr '\n' ':' | sed -e 's/:$//'`
+        export PYTHONPATH
+        LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | tr ':' '\n' | \
+            egrep -v "^($User_release_area|$MYSITEROOT/lhcb)" | \
+            tr '\n' ':' | sed -e 's/:$//'`
+        export LD_LIBRARY_PATH
+        exec env -u CMTCONFIG -u B2DXFITTERSROOT "$0" "$@"
+    fi
+    # automatic set up in standalone build mode
+    if test -z "$B2DXFITTERSROOT"; then
+        cwd="$(pwd)"
+        if test -z "$(dirname $0)"; then
+        # have to guess location of setup.sh
+        cd ../standalone
+        . ./setup.sh
+        cd "$cwd"
+        else
+        # know where to look for setup.sh
+        cd "$(dirname $0)"/../standalone
+        . ./setup.sh
+        cd "$cwd"
+        fi
+    unset cwd
+    fi
+fi
+
+# figure out which custom allocators are available
+# prefer jemalloc over tcmalloc
+for i in libjemalloc libtcmalloc; do
+    for j in `echo "$LD_LIBRARY_PATH" | tr ':' ' '` \
+        /usr/local/lib /usr/lib /lib; do
+        for k in `find "$j" -name "$i"'*.so.?' | sort -r`; do
+            if test \! -e "$k"; then
+            continue
+        fi
+        echo adding $k to LD_PRELOAD
+        if test -z "$LD_PRELOAD"; then
+            export LD_PRELOAD="$k"
+            break 3
+        else
+            export LD_PRELOAD="$LD_PRELOAD":"$k"
+            break 3
+        fi
+    done
+    done
+done
+
+# set batch scheduling (if schedtool is available)
+schedtool="`which schedtool 2>/dev/zero`"
+if test -n "$schedtool" -a -x "$schedtool"; then
+    echo "enabling batch scheduling for this job (schedtool -B)"
+    schedtool="$schedtool -B -e"
+else
+    schedtool=""
+fi
+
+# set ulimit to protect against bugs which crash the machine: 2G vmem max,
+# no more then 8M stack
+ulimit -v $((3072 * 1024))
+ulimit -s $((   8 * 1024))
+
+# trampoline into python
+exec $schedtool /usr/bin/time -v env python -O -- "$0" "$@"
 """
+__doc__ = """
 Calculate correction factors (k-factors) for partially reconstructed
 background to Bs⁰ → DsK.
 
@@ -8,13 +93,13 @@ background to Bs⁰ → DsK.
   - Bs → Ds⁽*⁾(π,ρ)
 
 """
-
 # Python standard libraries
 import os
 import sys
 from optparse import OptionParser
 
 # ROOT globals
+import B2DXFitters
 import ROOT
 from ROOT import gROOT
 gROOT.SetBatch(True)
@@ -111,7 +196,7 @@ def get_workspace(configname, varnames, masslo, masshi, debug):
 
     if configname.startswith('Bs2DsK'):
         hypo = TString("BsDsK")
-    else if configname.startswith('Bs2DsPi'):
+    elif configname.startswith('Bs2DsPi'):
         hypo = TString("BsDsPi")
     else:
         print 'Unknown mass hypothesis; job will crash.'
