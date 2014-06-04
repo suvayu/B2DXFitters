89002021ced2138ef09dd72a804906804e92551a
adudziak <adudziak@4525493e-7705-40b1-a816-d608a930855b>
2014-06-04 10:18:49 +0000

    config for toys
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Urania/trunk/PhysFit/B2DXFitters@173448 4525493e-7705-40b1-a816-d608a930855b

--- data/Bs2DsKConfigForNominalGammaFitToys5M_WithProdDetAsy.py
+++ data/Bs2DsKConfigForNominalGammaFitToys5M_WithProdDetAsy.py
@@ -3,7 +3,7 @@ def getconfig() :
     from Bs2DsKConfigForNominalGammaFitToys5M import getconfig as getconfig_nominal
     baselineconfig = getconfig_nominal()
 
-    baselineconfig["aprod_Signal"] = -0.014
-    baselineconfig["adet_Signal"]  = 0.01 
+    baselineconfig["aprod_Signal"] = -0.0175698 #-0.014
+    baselineconfig["adet_Signal"]  = 0.0100562 #0.01 
 
     return baselineconfig
