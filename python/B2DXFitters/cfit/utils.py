6bdc5353c6a39fe1083c1cae7f6f1c919e90018b
Suvayu Ali <Suvayu.Ali@cern.ch>
2014-08-16 14:42:20 +0200

    cfit.utils: utility to import attributes from namespace

--- python/B2DXFitters/cfit/utils.py
+++ python/B2DXFitters/cfit/utils.py
@@ -11,6 +11,25 @@ from ROOT import RooFit
 ws_import = getattr(ROOT.RooWorkspace, 'import')
 
 
+def _import_args(namespace, d = {}):
+    """Import attributes from namespace to local environment.
+
+    namespace -- namespace to import attributes from
+    d         -- dictionary that is returned with attributes
+                 and values (default: empty dict, leave it
+                 this way unless you know what you are doing)
+
+    Usage:
+      >>> opts = parser.parse_args(['foo', '-o', 'bar'])
+      >>> locals().update(_import_args(opts))
+
+    """
+    attrs = vars(namespace)
+    for attr in attrs:
+        d[attr] = getattr(namespace, attr)
+    return d
+
+
 def WS(ws, obj, opts = [RooFit.RecycleConflictNodes(), RooFit.Silence()]):
     """ "swallow" object into a workspace, returns swallowed object """
     name = obj.GetName()
