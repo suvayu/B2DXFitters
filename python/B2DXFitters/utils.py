20257be649c5f94ec1cdec28aaab69b20b882825
mschille <mschille@4525493e-7705-40b1-a816-d608a930855b>
2015-06-22 19:30:55 +0000

    cFit: factor out routines that are useful for tagging calibration
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Urania/trunk/PhysFit/B2DXFitters@190661 4525493e-7705-40b1-a816-d608a930855b

--- python/B2DXFitters/utils.py
+++ python/B2DXFitters/utils.py
@@ -62,3 +62,78 @@ def setConstantIfSoConfigured(config, obj, recache = {}):
         # ignore everything else
         pass
 
+def printPDFTermsOnDataSet(dataset, terms = []):
+    """
+    print the values of various terms in a pdf for each entry in a data set
+
+    dataset --  data set for which to print terms
+    terms --    python list of RooFit objects whose values should be printed
+                for each entry in the data set
+
+    This routine is mostly useful to debug cases where something in the PDF
+    calculation goes wrong (inside user C++ code, or because of invalid
+    entries in the data sample).
+    """
+    print 72 * '#'
+    print 'DEBUG: DUMPING TERMS FOR EACH ENTRY IN DATASET'
+    print 72 * '#'
+    vlist = [ v.clone(v.GetName()) for v in terms ]
+    for v in vlist:
+        ROOT.SetOwnership(v, True)
+        v.attachDataSet(dataset)
+    tmpvlist = {}
+    for v in vlist: tmpvlist[v.GetName()] = v
+    vlist = tmpvlist
+    del tmpvlist
+    notchanged = False
+    while not notchanged:
+        notchanged = True
+        for k in dict(vlist):
+            vs = vlist[k].getVariables()
+            ROOT.SetOwnership(vs, True)
+            it = vs.fwdIterator()
+            while True:
+                obj = it.next()
+                if None == obj: break
+                if obj.GetName() in vlist: continue
+                notchanged = False
+                vlist[obj.GetName()] = obj
+            vs = vlist[k].getComponents()
+            ROOT.SetOwnership(vs, True)
+            it = vs.fwdIterator()
+            while True:
+                obj = it.next()
+                if None == obj: break
+                if obj.GetName() in vlist: continue
+                notchanged = False
+                vlist[obj.GetName()] = obj
+        if not notchanged: break
+    obs = dataset.get()
+    obsdict = { }
+    it = obs.fwdIterator()
+    while True:
+        obj = it.next()
+        if None == obj: break
+        obsdict[obj.GetName()] = obj
+    for i in range(0, dataset.numEntries()):
+        dataset.get(i)
+        if dataset.isWeighted():
+            s = 'DEBUG: WEIGHT %g OBSERVABLES:' % dataset.weight()
+        else:
+            s = 'DEBUG: OBSERVABLES:'
+
+        for k in obsdict:
+            s = ('%s %s = %g' % (s, k, obsdict[k].getVal()) if
+                    obsdict[k].InheritsFrom('RooAbsReal') else
+                    '%s %s = %d' % (s, k, obsdict[k].getIndex()))
+        print s
+        vals = {}
+        for k in vlist:
+            vals[k] = (vlist[k].getValV(obs) if
+                    vlist[k].InheritsFrom('RooAbsReal') else vlist[k].getIndex())
+        for k in sorted(vals.keys()):
+            if k in obsdict: continue
+            print 'DEBUG:    ===> %s = %g' % (k, vals[k])
+    print 72 * '#'
+    return None
+
