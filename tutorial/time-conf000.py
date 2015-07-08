2dce7231a5b4175a87e1d39465a8c2543fba8ca6
mschille <mschille@4525493e-7705-40b1-a816-d608a930855b>
2015-07-08 22:35:45 +0000

    add tutorial subdirectory for example/teaching code
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Urania/trunk/PhysFit/B2DXFitters@191525 4525493e-7705-40b1-a816-d608a930855b

---
+++ tutorial/time-conf000.py
@@ -0,0 +1,15 @@
+{
+    'Debug': True,
+    'UseKFactor': False,
+    'ParameteriseIntegral': False,
+
+    'FitConfig': {
+        'Optimize': 1,
+        'Offset': True,
+        'NumCPU': 1,
+        'Strategy': 2,
+    },
+
+    'IsData': False,
+    'Blinding': True,
+}
