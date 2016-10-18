b143323c965db2cdd767f09c3debb9b820cf0292
ueitschb <ueitschb@4525493e-7705-40b1-a816-d608a930855b>
2016-10-18 16:34:44 +0000

    Updated Spline paramters of DsK Sfit Config to the correct ones
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Urania/trunk/PhysFit/B2DXFitters@208354 4525493e-7705-40b1-a816-d608a930855b

--- data/Bs2DsK_3fbCPV/Bs2DsK/Bs2DsKConfigForNominalMC.py
+++ data/Bs2DsK_3fbCPV/Bs2DsK/Bs2DsKConfigForNominalMC.py
@@ -2,13 +2,13 @@ def getconfig() :
 
     from Bs2DsKConfigForNominalMassFit import getconfig as getconfig_nominal
     configdict = getconfig_nominal()
-
+    
     from math import pi
 
     # PHYSICAL PARAMETERS
     configdict["Gammas"]        =  0.661376   # in ps^{-1}
     configdict["DeltaGammas"]   =  -0.10970
-    configdict["DeltaMs"]       = 20 #17.8   # in ps^{-1}
+    configdict["DeltaMs"]       = 17.8   # in ps^{-1}
     configdict["TagEffSig"]     = 0.403
     configdict["TagOmegaSig"]   = 0.391
     configdict["StrongPhase"]   = 20. / 180. * pi
@@ -21,14 +21,14 @@ def getconfig() :
     configdict["ConstrainsForTaggingCalib"] = False
     configdict["FixAcceptance"] = False
 
-    # Nominal MC Resolution and corresponding tagging parameters (Dspi)
+    # Nominal MC Resolution and corresponding tagging parameters (DsK)
     configdict["Resolution"] = { "scaleFactor":{"p0":0.0, "p1":1.201, "p2":0.0},
                                  "meanBias":0.0,
                                  "shape": { "sigma1":2.14946e-02, "sigma2":3.67643e-02, "sigma3":6.32869e-02,
                                             "frac1":3.72147e-01, "frac2":5.65150e-01},
                                  "templates": { "fileName":"../data/workspace/MDFitter/template_Data_Terr_Bs2DsPi_BDTGA.root",
                                                 "workName":"workspace",
-                                                "templateName":"TimeErrorPdf_Bs2DsPi"} }
+                                                "templateName":"TimeErrorPdf_Bs2DsK"} }
     configdict["TaggingCalibration"]["SS"] = {"p0": 0.4276, "dp0": 0.0, "p1": 1.184, "dp1": 0.0,
                                               "cov": [ [1.0, 0.0, 0.0, 0.0],
                                                        [0.0, 1.0, 0.0, 0.0],
@@ -44,25 +44,23 @@ def getconfig() :
 
 
     configdict["Acceptance"] = { "knots": [0.50, 1.0,  1.5, 2.0, 3.0, 12.0],
-                                 "values": [5.6512e-01, 6.3939e-01, 9.9391e-01, 1.0961e+00, 1.0567e+00, 1.2991e+00] }
+                                 "values": [4.4380e-01,6.5460e-01,8.9162e-01,1.0231e+00,1.1312e+00,1.2051e+00] }
 
 
     configdict["constParams"] = []
-    configdict["constParams"].append('Gammas_Bs2DsPi')
-    configdict["constParams"].append('deltaGammas_Bs2DsPi')
-    #configdict["constParams"].append('DeltaMs_Bs2DsPi')
-    configdict["constParams"].append('C_Bs2DsPi')
-    configdict["constParams"].append('Cbar_Bs2DsPi')
-    configdict["constParams"].append('S_Bs2DsPi')
-    configdict["constParams"].append('Sbar_Bs2DsPi')
-    configdict["constParams"].append('D_Bs2DsPi')
-    configdict["constParams"].append('Dbar_Bs2DsPi')
+    configdict["constParams"].append('Gammas_Bs2DsK')
+    configdict["constParams"].append('deltaGammas_Bs2DsK')
+    configdict["constParams"].append('DeltaMs_Bs2DsK')
+#    configdict["constParams"].append('C_Bs2DsK')
+#    configdict["constParams"].append('Cbar_Bs2DsK')
+#    configdict["constParams"].append('S_Bs2DsK')
+#    configdict["constParams"].append('Sbar_Bs2DsK')
+#    configdict["constParams"].append('D_Bs2DsK')
+#    configdict["constParams"].append('Dbar_Bs2DsK')
     configdict["constParams"].append('tagEff_OS')
     configdict["constParams"].append('tagEff_SS')
     configdict["constParams"].append('aTagEff_OS')
     configdict["constParams"].append('aTagEff_SS')
-    configdict["constParams"].append('aTagEff_Both')
-
     if configdict["FixAcceptance"] == True:
         configdict["constParams"].append('var1')
         configdict["constParams"].append('var2')
@@ -75,7 +73,6 @@ def getconfig() :
         configdict["constParams"].append('p0_SS')
         configdict["constParams"].append('p1_OS')
         configdict["constParams"].append('p1_SS')
-
     configdict["constParams"].append('dp0_OS')
     configdict["constParams"].append('dp0_SS')
     configdict["constParams"].append('dp1_OS')
