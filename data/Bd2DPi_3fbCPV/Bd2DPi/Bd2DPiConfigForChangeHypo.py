21ac42298bdbf08ae1f8165343ba3f99bc9d2dd2
vibattis <vibattis@4525493e-7705-40b1-a816-d608a930855b>
2016-10-27 17:05:20 +0000

    Change mass hypo config for Bd updated
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Urania/trunk/PhysFit/B2DXFitters@208438 4525493e-7705-40b1-a816-d608a930855b

--- data/Bd2DPi_3fbCPV/Bd2DPi/Bd2DPiConfigForChangeHypo.py
+++ data/Bd2DPi_3fbCPV/Bd2DPi/Bd2DPiConfigForChangeHypo.py
@@ -10,23 +10,29 @@ def getconfig() :
     # PrefixID's: all the following fields are mandatory (choose either ID or newID for each particle)
     # The number of Bachelors, Charm particles and Charm children is flexible
     configdict["Charm1ChildrenPrefix"] = { "Child1"   : {"Name": "lab0_FitDaughtersConst_Dplus_P0",
-                                                         "ID": "K"},
+                                                         "ID": "K",
+                                                         "Type": "F"},
 
                                            "Child2"   : {"Name": "lab0_FitDaughtersConst_Dplus_P1",
-                                                         "ID": "Pi"},
+                                                         "ID": "Pi",
+                                                         "Type": "F"},
 
                                            "Child3"   : {"Name": "lab0_FitDaughtersConst_Dplus_P2",
-                                                         "ID": "Pi"}
+                                                         "ID": "Pi",
+                                                         "Type": "F"}
                                            }
     
     configdict["BeautyChildrenPrefix"] = { "Bachelor1" : {"Name": "lab0_FitDaughtersConst_P0",
-                                                          "newID": "K"},
+                                                          "newID": "K",
+                                                          "Type": "F"},
                                            
                                            "Charm1"    : {"Name": "lab0_FitDaughtersConst_Dplus",
-                                                          "ID": "D"}
+                                                          "ID": "F",
+                                                          "Type": "F"}
                                            }
     
-    configdict["BeautyPrefix"] = { "Name": "lab0_FitDaughtersConst" }
+    configdict["BeautyPrefix"] = { "Name": "lab0_FitDaughtersConst",
+                                   "Type": "F"}
 
     # Handle multiple candidates (if any). Comment out if not needed
     #configdict["Index"] = "lab0_FitDaughtersConst_nPV"
